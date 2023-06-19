e3b395a910fec1415836d1ae57de8bd7304b8997
Nerd <imalonelynerd@gmail.com>
2023-06-20 01:10:55 +0200

    Woops ! Forgor other things lol

---
+++ js/effects.js
@@ -0,0 +1,19 @@
+function eff1(){
+    let thingy = document.getElementById("eff1");
+    thingy.innerHTML = thingy.innerHTML === "LunarRatOS"?"[LunarRatOS]":"LunarRatOS";
+}
+
+function eff2(){
+    let thingy = document.getElementById("eff1");
+    thingy.style.opacity = thingy.style.opacity === "0.7" ?"1":"0.7";
+}
+
+function eff3(){
+    let thingy = document.getElementById("eff1");
+    thingy.innerHTML = thingy.innerHTML === "LunarRatOS"?"":"LunarRatOS";
+}
+
+function eff4() {
+    let thingy = document.getElementById("eff1");
+    thingy.innerHTML = thingy.innerHTML === "LunarRatOS"?"WOW !":"LunarRatOS";
+}
